b9a43b349123e1ee1bf26c7575f91e31da01599a
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-11-06 00:43:46 +0500

    Task2

---
+++ Tyuiu.AlbornozJ.Sprint4.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿
+using Tyuiu.AlbornozJ.Sprint4.Task2.V24.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint4.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9 };
+            int res = ds.Calculate(numsArray);
+            int wait = 42;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint4.Task2.V24/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.AlbornozJ.Sprint4.Task2.V24.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Random rnd = new Random();
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество элементов массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] numsArray = new int[len];
+for (int i = 0; i <= len - 1; i++)
+{
+    numsArray[i] = rnd.Next(4, 9);
+}
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.Write(numsArray[i] + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(numsArray);
+
+Console.WriteLine("Сумма нечетных элементов массива = " + res);
+Console.ReadKey();
