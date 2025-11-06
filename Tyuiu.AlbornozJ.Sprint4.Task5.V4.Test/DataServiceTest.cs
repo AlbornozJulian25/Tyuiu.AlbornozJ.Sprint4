6d2cb798d837c09d15f5bd36d0a7df78d1e532ee
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-11-06 23:14:28 +0500

    Task5

---
+++ Tyuiu.AlbornozJ.Sprint4.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿
+using Tyuiu.AlbornozJ.Sprint4.Task5.V4.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint4.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { -4, -3, -2, -1,  0 },
+                                          { 1, 2, 3, 4, 5 },
+                                          { -1, -2, 6, -3, -4 },
+                                          { 0, 1, 2, 3, 4 },
+                                          { -4, -3, 5, 6, -2 } };
+
+            int res = ds.Calculate(mas2);
+            int wait = 12;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint4.Task5.V4/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AlbornozJ.Sprint4.Task5.V4.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+Random rnd = new Random();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+Console.WriteLine("*******************************");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = rnd.Next(-4, 7);
+    }
+}
+
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+int res = ds.Calculate(matrix);
+Console.WriteLine("Количество положительных элементов = " + res);
+Console.ReadKey();
