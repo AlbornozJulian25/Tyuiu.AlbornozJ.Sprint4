43d607f18cc7461d8fe25a28a4f993aa19b90f19
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-11-06 21:53:51 +0500

    Task3

---
+++ Tyuiu.AlbornozJ.Sprint4.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AlbornozJ.Sprint4.Task3.V2.Lib
+{
+    public class DataService : ISprint4Task3V2
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int maxElement = matrix[0, 0];
+                        
+            for (int j = 0; j < columns; j++)
+            {
+                if (matrix[0, j] > maxElement)
+                {
+                    maxElement = matrix[0, j];
+                }
+            }
+            return maxElement;
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint4.Task3.V2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AlbornozJ.Sprint4.Task3.V2.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[,] matrix = new int[5, 5] { { 4, 3, 2, 8, 7 },
+                                { 4, 6, 5, 5, 7 },
+                                { 3, 8, 8, 3, 5 },
+                                { 5, 7, 5, 4, 4 },
+                                { 8, 8, 3, 7, 6 } };
+
+int rows = matrix.GetUpperBound(0) + 1;
+int columns = matrix.Length / rows;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(matrix);
+
+Console.WriteLine("Максимальный элемент в первой строке = " + res);
+Console.ReadKey();
