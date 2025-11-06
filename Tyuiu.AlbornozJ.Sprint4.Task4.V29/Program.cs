bae3b9a4d902b1b2e5bb841026b8fdf0e63614ec
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-11-06 22:30:28 +0500

    Task4

---
+++ Tyuiu.AlbornozJ.Sprint4.Task4.V29/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AlbornozJ.Sprint4.Task4.V29.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+Console.WriteLine("*******************************");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"Введите {i},{j} элемент массива: ");
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+int res = ds.Calculate(matrix);
+Console.WriteLine("Сумма четных элементов массива = " + res);
+Console.ReadKey();
